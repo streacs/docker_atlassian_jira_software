fd2c48a79ee0f3c900a73b493da80fff4788caa8
Oliver Wolf <root@streacs.com>
2017-12-30 23:31:24 +0100

    added more directory tests

--- rspec-specs/spec/localhost/server_spec.rb
+++ rspec-specs/spec/localhost/server_spec.rb
@@ -6,3 +6,15 @@ describe file('/home/jira') do
   it { should be_owned_by 'jira' }
   it { should be_grouped_into 'jira' }
 end
+
+describe file('/opt/atlassian/jira') do
+  it { should be_directory }
+  it { should be_owned_by 'jira' }
+  it { should be_grouped_into 'jira' }
+end
+
+describe file('/var/opt/atlassian/application-data/jira') do
+  it { should be_directory }
+  it { should be_owned_by 'jira' }
+  it { should be_grouped_into 'jira' }
+end
